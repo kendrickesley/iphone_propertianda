a5c42b1c1bba5ba98095db45842245b40c220d7c
Kendrick <kendrick.kesley1995@gmail.com>
2017-08-19 16:33:09 +1000

    Initial Commit

---
+++ Properti Anda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Properti Anda
+//
+//  Created by Kendrick on 19/8/17.
+//  Copyright © 2017 Kendrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
